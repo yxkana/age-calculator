715360bc4efe0667ce051662fde380e653a9a85b
Kanna <yxkana52@gmail.com>
2023-06-29 10:54:12 +0200

    update 1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Poppins } from "next/font/google";
+
 import "~/styles/globals.css";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700", "800"],
+  style: ["italic", "normal"],
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default MyApp;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(259, 100%, 65%)",
+        error: "hsl(0, 100%, 67%)"
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
